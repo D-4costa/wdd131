9264914c7a815de8b9280ed00cb32e967be26c53
D-4costa <danielleelisabeth01@gmail.com>
2025-09-03 14:39:51 -0500

    Add files via upload

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+// Get current year
+const yearSpan = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+// Get last modified date
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;
