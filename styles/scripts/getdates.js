ab4c16d7faf28a49927fbd6220d81af2d70e825f
D-4costa <danielleelisabeth01@gmail.com>
2025-09-03 14:39:25 -0500

    Delete styles/scripts directory

--- styles/scripts/getdates.js
+++
@@ -1,8 +0,0 @@
-// Get current year
-const yearSpan = document.getElementById("currentyear");
-const currentYear = new Date().getFullYear();
-yearSpan.textContent = currentYear;
-
-// Get last modified date
-const lastModified = document.lastModified;
-document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;
